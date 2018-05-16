a9ebff4cfcd449f83296703657905ae61f41665d
Yuta Hiroto <git@about-hiroppy.com>
2018-05-16 10:12:42 +0900

    Updates

---
+++ precache-manifest.8a499b0691f4c6ad7bace8c03158a6ba.js
@@ -0,0 +1,40 @@
+self.__precacheManifest = [
+  {
+    "url": "674f50d287a8c48dc19ba404d20fe713.eot"
+  },
+  {
+    "revision": "6a5f74e69b7a73807974e6addaa690e7",
+    "url": "index.html"
+  },
+  {
+    "url": "fee66e712a8a08eef5805a46892932ad.woff"
+  },
+  {
+    "url": "b06871f281fee6b241d60582ae9369b9.ttf"
+  },
+  {
+    "url": "af7ae505a9eed503f8b8e6982036873e.woff2"
+  },
+  {
+    "revision": "f5ce8976e88cd5e3603e",
+    "url": "a97128d1e5.css"
+  },
+  {
+    "url": "9abae3ddb874ce096fff.js"
+  },
+  {
+    "url": "760bd83ee04dff470e0277f3eb7deebe.svg"
+  },
+  {
+    "url": "2.9abae3ddb874ce096fff.js"
+  },
+  {
+    "url": "1.9abae3ddb874ce096fff.js"
+  },
+  {
+    "url": "06a6ac1709970857f419dc78aca05353.ttf"
+  },
+  {
+    "url": "0.9abae3ddb874ce096fff.js"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.2.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.64b53a33c4a16cb47178c6710844ecff.js"
+  "precache-manifest.8a499b0691f4c6ad7bace8c03158a6ba.js"
 );
 
 /**
